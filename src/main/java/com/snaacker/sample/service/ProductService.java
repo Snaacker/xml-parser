6e20ccc4f011c1ec670b6d81ec46c0f2c4440fe0
Huy Tran <quanghuy.hut.fami@gmail.com>
2023-10-02 23:54:09 +0200

    Fix spotless check

--- src/main/java/com/snaacker/sample/service/ProductService.java
+++ src/main/java/com/snaacker/sample/service/ProductService.java
@@ -24,80 +24,81 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ProductService {
-  Logger logger = LoggerFactory.getLogger(ProductService.class);
+    Logger logger = LoggerFactory.getLogger(ProductService.class);
 
-  public List<ProductResponse> getProductsByProductFeedId(long productFeedId) {
-    return null;
-  }
-
-  public String loadProducts(MultipartFile multipartFile) throws IOException {
-    String fileName =
-        StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
-    String uploadFile = saveFile(fileName, multipartFile);
-//    readFile(uploadFile);
-    return "Done upload";
-  }
-
-  private void readFile(String filename) {
-    File xmlFile = new File(filename);
-
-    JAXBContext jaxbContext;
-    try {
-      jaxbContext = JAXBContext.newInstance(Result.class);
-      Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-      Result result = (Result) jaxbUnmarshaller.unmarshal(xmlFile);
-      for (Product product : result.getProducts().getProduct()) {
-        System.out.println(product.getName());
-      }
-    } catch (JAXBException e) {
-      logger.error(e.getMessage());
-      throw new RuntimeException(e.getMessage());
-    }
-  }
-
-  private String saveFile(String fileName, MultipartFile multipartFile) throws IOException {
-    Path uploadPath = Paths.get("files-upload");
-
-    if (!Files.exists(uploadPath)) {
-      Files.createDirectories(uploadPath);
+    public List<ProductResponse> getProductsByProductFeedId(long productFeedId) {
+        return null;
     }
 
-    String fileCode = randomAlphanumeric();
-    String serverFileName = fileCode + "-" + fileName;
-    try (InputStream inputStream = multipartFile.getInputStream()) {
-      Path filePath = uploadPath.resolve(serverFileName);
-      Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-    } catch (IOException ioe) {
-      throw new IOException("Could not save file: " + fileName, ioe);
+    public String loadProducts(MultipartFile multipartFile) throws IOException {
+        String fileName =
+                StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        String uploadFile = saveFile(fileName, multipartFile);
+        //    readFile(uploadFile);
+        return "Done upload";
     }
 
-    File xmlFile = new File("files-upload/" + serverFileName);
+    private void readFile(String filename) {
+        File xmlFile = new File(filename);
 
-    JAXBContext jaxbContext;
-    try {
-      jaxbContext = JAXBContext.newInstance(Result.class);
-      Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-      Result result = (Result) jaxbUnmarshaller.unmarshal(xmlFile);
-      for (Product product : result.getProducts().getProduct()) {
-        System.out.println(product.getName());
-      }
-    } catch (JAXBException e) {
-      logger.error(e.getMessage());
-      throw new RuntimeException(e.getMessage());
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(Result.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Result result = (Result) jaxbUnmarshaller.unmarshal(xmlFile);
+            for (Product product : result.getProducts().getProduct()) {
+                System.out.println(product.getName());
+            }
+        } catch (JAXBException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
     }
-    return fileCode + "-" + fileName;
-  }
 
-  private String randomAlphanumeric() {
-    int leftLimit = 97; // letter 'a'
-    int rightLimit = 122; // letter 'z'
-    int targetStringLength = 10;
-    Random random = new Random();
-    StringBuilder buffer = new StringBuilder(targetStringLength);
-    for (int i = 0; i < targetStringLength; i++) {
-      int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
-      buffer.append((char) randomLimitedInt);
+    private String saveFile(String fileName, MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get("files-upload");
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        String fileCode = randomAlphanumeric();
+        String serverFileName = fileCode + "-" + fileName;
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(serverFileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save file: " + fileName, ioe);
+        }
+
+        File xmlFile = new File("files-upload/" + serverFileName);
+
+        JAXBContext jaxbContext;
+        try {
+            jaxbContext = JAXBContext.newInstance(Result.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Result result = (Result) jaxbUnmarshaller.unmarshal(xmlFile);
+            for (Product product : result.getProducts().getProduct()) {
+                System.out.println(product.getName());
+            }
+        } catch (JAXBException e) {
+            logger.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+        return fileCode + "-" + fileName;
     }
-    return buffer.toString();
-  }
+
+    private String randomAlphanumeric() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt =
+                    leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
+    }
 }
