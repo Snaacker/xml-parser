8b80d65771b28e8b2aa43059f7242e21f011ae32
Huy Tran <quanghuy.hut.fami@gmail.com>
2023-10-05 22:00:14 +0200

    Update local startup script

--- local-startup.sh
+++ local-startup.sh
@@ -5,7 +5,8 @@ if [ ! "$(docker ps -q -f name=db)" ]; then
     docker run --name db -p 3306:3306 -e MYSQL_USER=admin \
     -e MYSQL_PASSWORD=admin -e MYSQL_ROOT_PASSWORD=password \
     -e MYSQL_DATABASE=xmlparser -d mysql:8.1
-
+echo "Wait for DB startup"
+sleep 30
 fi
 
 export DATABASE_URL_CUSTOMIZE="jdbc:mysql://localhost:3306/xmlparser"
